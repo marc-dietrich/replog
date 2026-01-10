e74942d9cb1e5ad0917b423f40758fbdd3fa6eb5
marc-dietrich <mc.dietrich@gmx.d>
2026-01-10 12:23:19 +0100

    pwa for andoid

--- src/main.jsx
+++ src/main.jsx
@@ -11,3 +11,12 @@ createRoot(document.getElementById("root")).render(
 
   </StrictMode>
 );
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw.js")
+      .then((reg) => console.log("Service Worker registriert:", reg))
+      .catch((err) => console.error("Service Worker Registrierung fehlgeschlagen:", err));
+  });
+}
+
